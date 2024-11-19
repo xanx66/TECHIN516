c6c1b4077e85f9ce49a7a10b698d1cec35074980
jack <42076119+htchr@users.noreply.github.com>
2024-11-19 11:32:18 -0800

    fix kinova install

--- kinova_ws/install_kinova_dependencies.sh
+++ kinova_ws/install_kinova_dependencies.sh
@@ -8,6 +8,9 @@ source /opt/ros/humble/setup.bash
 # install python packages
 sudo apt install -y python3-pip
 pip install matplotlib
+pip install pandas
+pip install numpy
+pip install pyquaternion
 
 # install helpful tools
 sudo apt install -y \
@@ -21,31 +24,43 @@ sudo apt install -y \
 echo ". /usr/share/gazebo/setup.sh" >> ~/.bashrc
 
 # install kinova dependencies
-sudo apt install -y \
+sudo apt update && sudo apt install -y \
     python3-colcon-common-extensions \
     python3-colcon-mixin \
     python3-colcon-clean \
     python3-vcstool \
-    ros-humble-moveit
+    ros-humble-moveit \
+    ros-humble-realtime-tools \ 
 pip install moveit_config_utils
-pip install pandas
-pip install pyquaternion
 
 # create src directory
 mkdir src
 
-# install kinova packages
+# # ros2 control
+sudo apt-get install -y wget
+vcs import --input https://raw.githubusercontent.com/ros-controls/ros2_control_ci/master/ros_controls.$ROS_DISTRO.repos src
+rosdep update --rosdistro=$ROS_DISTRO
+
+# # install kinova packages
 git clone https://github.com/Kinovarobotics/ros2_kortex.git src/ros2_kortex
 vcs import src --skip-existing --input src/ros2_kortex/ros2_kortex.$ROS_DISTRO.repos
 vcs import src --skip-existing --input src/ros2_kortex/ros2_kortex-not-released.$ROS_DISTRO.repos
 vcs import src --skip-existing --input src/ros2_kortex/simulation.humble.repos
+cd src
+rm -rf control_msgs gz_ros2_control ros2_controllers gazebo_ros2_control ros2_control 
+cd ..
+sudo apt-get update
+rosdep install --ignore-src --from-paths src -y -r
 
 # install pymoveit2
-cd src
-git clone https://github.com/AndrejOrsula/pymoveit2.git
-rosdep install -y -r -i --rosdistro ${ROS_DISTRO} --from-paths ./pymoveit2
-cd ..
+ cd src
+ git clone https://github.com/AndrejOrsula/pymoveit2.git
+ rosdep install -y -r -i --rosdistro ${ROS_DISTRO} --from-paths ./pymoveit2
+ cd ..
 
-rosdep install --ignore-src --from-paths src -y -r
-colcon build --merge-install --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 3
+# build
+source /opt/ros/humble/setup.bash
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 3
+# colcon build --symlink-install --parallel-workers 3
+rm ros_controls.humble.repos
 source install/setup.bash
